bec7a96ab9c4309ebe6ddb36b5cc0b89dbcad323
frosskiel <frosskiel@gmail.com>
2024-12-10 09:42:19 -0300

    fix: enviar observacion de item al back

--- src/GServicios/Servicio.jsx
+++ src/GServicios/Servicio.jsx
@@ -165,6 +165,7 @@ export default function Servicio({ title }) {
       listaItems: servicios.map((item) => ({
         ...item,
         tipoServicioId: item.tipoServicioId,
+        observacion: item.observaciones
       })),
     };
 
